f75923efd3194565e29b01eaa103572c46360082
jimmy <jimmy70750@gmail.com>
2018-05-27 21:10:08 +0800

    指向非常數資料的常數指標

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 int array[10]={1,7,5,3,9,2,10,8,4,6};
 int i;
-int bubbleSort(int v[]);                //指向非常數資料的非常數指標
+int bubbleSort(int *const v[]);                //指向非常數資料的常數指標
 int main() {
 	bubbleSort(array);                  //泡沫排列 
 	for(i=0;i<10;i++){
